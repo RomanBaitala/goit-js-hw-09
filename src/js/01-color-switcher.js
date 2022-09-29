2de65cf35f6d8e1939d29a4bdaac67604de2b1e9
Roman <roman.baytala@gmail.com>
2022-09-29 10:42:01 +0300

    hw-p-1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,28 @@
+const startBtnRef = document.querySelector('button[data-start]');
+const stopBtnRef = document.querySelector('button[data-stop]');
+const bodyRef = document.querySelector('body');
+stopBtnRef.disabled = true;
+
+startBtnRef.addEventListener('click', onButtonStart);
+stopBtnRef.addEventListener('click', onButtonStop);
+
+function onButtonStart(){
+    randomBg = setInterval(() => {
+        bodyRef.style.backgroundColor = `${getRandomHexColor()}`;
+    }, 1000);
+    toggleActiveBtn(stopBtnRef, startBtnRef);
+};
+
+function onButtonStop(){
+    clearInterval(randomBg);
+    toggleActiveBtn(startBtnRef, stopBtnRef);
+};
+
+function toggleActiveBtn(activeBtn, inactiveBtn) {
+    inactiveBtn.disabled = true;
+    activeBtn.disabled = false;
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
---
+++ src/js/02-timer.js
@@ -0,0 +1,7 @@
+import flatpickr from "flatpickr";
+
+
+flatpickr('#datetime-picker', {
+    enableTime: true,
+    dateFormat: "Y-m-d H:i",
+})
