dc79562458c7d4a80fb9886e9e8a4b7f7cd61c58
RomanBaitala <63609124+RomanBaitala@users.noreply.github.com>
2022-09-29 08:01:22 +0000

    Deploying to gh-pages from @ RomanBaitala/goit-js-hw-09@16205d544371aaa62394f695548afee706c066df 🚀

---
+++ 01-color-switcher.5cac1f96.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");function o(t,e){e.disabled=!0,t.disabled=!1}e.disabled=!0,t.addEventListener("click",(function(){setInterval((()=>{n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3);o(e,t)})),e.addEventListener("click",(function(n){clearInterval(n),o(t,e)}));
+//# sourceMappingURL=01-color-switcher.5cac1f96.js.map
